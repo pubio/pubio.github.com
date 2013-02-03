675e7cf79dbfea72f49c899479d474a8471c991f
Paul Lang <paul.andrew.lang@gmail.com>
2013-02-02 22:07:53 -0800

    Removed pusher include

--- js/controllers.js
+++ js/controllers.js
@@ -5,11 +5,14 @@ function RecommendationListCtrl($scope, $http) {
     $scope.recommendations = data.recommendations;
   });
 
-
   $http.jsonp('http://secure-shelf-4168.herokuapp.com/topRecommendations/?callback=JSON_CALLBACK').success(function(data) {
     $scope.topRecommendations = data.topRecommendations;
   });
-
 }
 
 
+function LoginCtrl($scope, $http) {
+  $http.jsonp('http://secure-shelf-4168.herokuapp.com/session/?callback=JSON_CALLBACK').success(function(data) {
+    $scope.recommendations = data.recommendations;
+  });
+}
